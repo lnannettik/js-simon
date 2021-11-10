b6c6c79623ba20620d7f4d93df48e0d1f3a9f66e
lnannettik <l.nannetti.k@gmail.com>
2021-11-10 19:36:01 +0100

    push

--- main.js
+++ main.js
@@ -11,52 +11,71 @@ console.log(`JS OK`);
 
 let numeriRandom = [];
 
-let random = '';
-// let numeriUtente = [];
+let numeriUtente = [];
 
-
+// genera i numeri iniziali usando la funzione getRandomNumber()
 for (let i = 0; i < 5; i++) {
-    random += [getRandomNumber()];   
-    numeriRandom.push('random')
-    // console.log(random)   
-    console.log(random)
+    numeriRandom.push(getRandomNumber())
 }    
+
+document.getElementById('display').innerHTML = `i tuoi numeri sono ${numeriRandom}`; 
 
 
 // timer 30 sec
-
 const countDown = document.getElementById('countDown');
-let second = 2; // **********  30
+let second = 5; // **********  30
 
 
 const timer = setInterval (() => {
+
+    // se il timer è < 3 cambia lo sfondo in red
+    if((second <= 3) && (second > 0)) {
+        document.querySelector('.body').classList.add('bgc-red')
+
+        // elimina lo sfondo red dopo 30 sec
+        const timerScnd = setTimeout (() => {
+            document.querySelector('.body').classList.remove('bgc-red')
+        }, 500);
+    }
+
+    // se il timer arriva a 0 fermati
     if(second === 0) {
         // stop the count
         clearInterval(timer)
-        // add .d-none to counDown
+        // add .d-none to #counDown
         document.getElementById('countDown').classList.add('d-none')
+        // add .d-none to #display
+        document.getElementById('display').classList.add('d-none')
 
+
+        // l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
+
+        let numeriUtente = []
+
+        for (let i = 0; i < 5; i++) {
+            // let numeriUtente = []
+            const insert = parseInt(prompt('Te li ricordi? Inserisci uno alla volta i numeri che ti ricordi!'))
+            numeriUtente.push(insert)
+            
+            console.log(insert)
+        }
+        
+        console.log(numeriUtente)
+
+
+        // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+        
+        
     } else {
         // mostra il conteggio
         countDown.innerText = second;
         // sottrai 1 a second
         second--;
-    } 
+    }     
     
-    // if((second <= 3) && (second > 0)) {
-    //     const timerScnd = setInterval (() => {
-    //         document.querySelector('.body').classList.add('bgc-red')
-    //     } 
-        
-    //     , 500);
-    
-}, 1000);
+}, 1000);    
 
-
-// l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt()
-
-const insert = parseInt(prompt('Te li ricordi? Inserisci uno alla volta i numeri che ti ricordi! Primo...'))
-console.log(insert)
 
 
 
